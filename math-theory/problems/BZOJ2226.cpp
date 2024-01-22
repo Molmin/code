d146e369dbfd67d52c276b0d6b1d16b6ae5cae29
Milmon <milmon@qq.com>
2024-01-22 14:02:23 +0800

    feat: done BZOJ2226

---
+++ math-theory/problems/BZOJ2226.cpp
@@ -0,0 +1,38 @@
+/* 
+ * @date: 2024.01.22
+ * @problem: BZOJ2226
+ * @algo: 莫比乌斯函数
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool vis[1000001];
+int cntPrime,prime[1000001];
+long long f[1000001];
+
+void precalc(int n=1000000){
+    f[1]=1;
+    for(int i=2;i<=n;i++){
+        if(!vis[i])prime[++cntPrime]=i,f[i]=1+i*(i-1ll);
+        for(int j=1;j<=cntPrime&&i*prime[j]<=n;j++){
+            vis[i*prime[j]]=true;
+            if(i%prime[j]==0){
+                f[i*prime[j]]=f[i]+(f[i]-f[i/prime[j]])*prime[j]*prime[j];
+                break;
+            }
+            f[i*prime[j]]=f[i]*f[prime[j]];
+        }
+    }
+}
+
+int main(){
+    precalc();
+    int T; scanf("%d",&T);
+    while(T--){
+        int n;
+        scanf("%d",&n);
+        printf("%lld\n",(f[n]+1)*n/2);
+    }
+    return 0;
+}
