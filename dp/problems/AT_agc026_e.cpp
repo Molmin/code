5773ba3f310d06c47329e19138f312b004078079
Milmon <milmon@qq.com>
2024-02-18 19:14:52 +0800

    feat: done AT_agc026_e

--- dp/problems/AT_agc026_e.cpp
+++ dp/problems/AT_agc026_e.cpp
@@ -7,12 +7,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n,posa[3001],posb[3001];
-string s,dp[3001];
+int n,posa[3002],posb[3002],rnk[6001];
+string s,dp[3002];
 
 int main(){
     scanf("%d",&n),cin>>s,s=" "+s;
+    int a=0,b=0;
     for(int i=1;i<=(n<<1);i++)
-        
+        if(s[i]=='a')rnk[i]=++a,posa[a]=i;
+        else rnk[i]=++b,posb[b]=i;
+    dp[n+1]="";
+    rnk[2*n+1]=n+1,posa[n+1]=posb[n+1]=2*n+1;
+    for(int i=n;i>=1;i--){
+        if(posa[i]<posb[i]){
+            int x=posb[i]; while(s[x]=='b')x++;
+            dp[i]="ab"+dp[rnk[x]];
+        }
+        else{
+            int l=posb[i]+1,r=posa[i];
+            dp[i]="b";
+            while(l<=r){
+                if(s[l]=='b')r=max(r,posa[rnk[l]]);
+                if(s[l]!='a'||posb[rnk[l]]>=posb[i])dp[i]+=s[l];
+                l++;
+            }
+            dp[i]+=dp[rnk[r+1]];
+        }
+        if(dp[i+1]>dp[i])dp[i]=dp[i+1];
+    }
+    cout<<dp[1]<<endl;
     return 0;
 }
